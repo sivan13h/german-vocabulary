05520d305b80e2ce89d61082b2d9479443fc45d3
סיון הרט <sivan13h@gmail.com>
2020-11-14 17:52:16 +0100

    finish PWA

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'news-v1';
+const staticAssets = [
+    './',
+    './index.html',
+    './style.css',
+    './style.css.map',
+    './style.scss',
+    './index.js',
+    './manifest.webmanifest',
+]
+
+self.addEventListener('install', async e => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+    return self.skipWaiting();
+})
+
+self.addEventListener('activate', e => {
+    self.clients.claim()
+})
+
+self.addEventListener('fetch',() => console.log("fetch"));
