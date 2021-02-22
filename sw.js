e98949756aebd2a7dbfe5c20c3c640e0f9932466
Sivan Hart <sivan13h@gmail.com>
2021-02-22 18:44:09 +0100

    add fetch to sw

--- sw.js
+++ sw.js
@@ -1,20 +1,24 @@
-const cacheName = 'news-v1';
+const cacheName = "news-v1";
 const staticAssets = [
-    './',
-    './index.html',
-    './style.css',
-    './style.css.map',
-    './style.scss',
-    './index.js',
-    './manifest.webmanifest',
-]
+  "./",
+  "./index.html",
+  "./style.css",
+  "./style.css.map",
+  "./style.scss",
+  "./index.js",
+  "./manifest.webmanifest",
+];
 
-self.addEventListener('install', async e => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
-    return self.skipWaiting();
-})
+self.addEventListener("install", async (e) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
 
-self.addEventListener('activate', e => {
-    self.clients.claim()
-})
+self.addEventListener("activate", (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  // it can be empty if you just want to get rid of that error
+});
